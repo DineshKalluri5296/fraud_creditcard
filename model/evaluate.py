ebe5019068db6b4da65d1993c61735af3ae2cf96
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2026-08-04 11:13:23 +0530

    Update evaluate.py

--- model/evaluate.py
+++ model/evaluate.py
@@ -1,6 +1,8 @@
 import os
+import json
 import joblib
 import pandas as pd
+
 from sklearn.metrics import accuracy_score
 
 MODEL_PATH = "model/model.pkl"
@@ -16,17 +18,24 @@ model = joblib.load(MODEL_PATH)
 df = pd.read_csv("data/card_transdata.csv")
 
 X = df.drop("fraud", axis=1)
+
 y = df["fraud"]
 
 pred = model.predict(X)
 
 accuracy = accuracy_score(y, pred)
 
-print(f"Accuracy : {accuracy}")
+print(f"Accuracy : {accuracy:.4f}")
+
+with open("model/model_metrics.json", "w") as f:
+    json.dump({"accuracy": float(accuracy)}, f, indent=4)
 
 if accuracy < THRESHOLD:
-    print("Accuracy dropped")
+
+    print("Accuracy dropped below threshold")
+
     exit(1)
 
 print("Accuracy acceptable")
+
 exit(0)

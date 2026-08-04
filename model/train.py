31353190242d03f14394d52c8f83b06ba4a01faa
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2026-08-04 11:12:49 +0530

    Update train.py

--- model/train.py
+++ model/train.py
@@ -14,6 +14,21 @@ X = df.drop("fraud", axis=1)
 # Target
 y = df["fraud"]
 
+training_stats = {}
+
+for col in X.columns:
+    training_stats[col] = {
+        "mean": float(X[col].mean()),
+        "std": float(X[col].std())
+    }
+
+with open("model/training_stats.json", "w") as f:
+    json.dump(training_stats, f, indent=4)
+
+print("Training statistics saved.")
+
+
+
 # Split
 X_train, X_test, y_train, y_test = train_test_split(
     X,
@@ -32,13 +47,22 @@ model = LGBMClassifier(
 
 # Train
 model.fit(X_train, y_train)
-
+print("Model training completed.")
 # Prediction
 pred = model.predict(X_test)
 
 acc = accuracy_score(y_test, pred)
 
 print(f"Accuracy : {acc:.4f}")
+
+    "accuracy": float(accuracy)
+}
+
+with open("model/model_metrics.json", "w") as f:
+    json.dump(metrics, f, indent=4)
+
+print("Model metrics saved.")
+
 
 # Save Model
 joblib.dump(model, "model/model.pkl")

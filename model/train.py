83ba4884c5d7ba965d6861b0dc87024b3d3d083c
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2026-08-04 11:50:46 +0530

    Update train.py

--- model/train.py
+++ model/train.py
@@ -14,21 +14,6 @@ X = df.drop("fraud", axis=1)
 # Target
 y = df["fraud"]
 
-training_stats = {}
-
-for col in X.columns:
-    training_stats[col] = {
-        "mean": float(X[col].mean()),
-        "std": float(X[col].std())
-    }
-
-with open("model/training_stats.json", "w") as f:
-    json.dump(training_stats, f, indent=4)
-
-print("Training statistics saved.")
-
-
-
 # Split
 X_train, X_test, y_train, y_test = train_test_split(
     X,
